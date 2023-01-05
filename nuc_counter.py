0ae97504fc5d4b725105c36b0f367a01c100abd3
Jandhyala Sai Krishna <119666181+krishnajandhyala@users.noreply.github.com>
2023-01-05 17:34:22 +0530

    Delete nuc_counter.py

--- nuc_counter.py
+++
@@ -1,9 +0,0 @@
-file = input("Enter your file path\t:\t")
-string = open(file).readline()
-
-A = string.count("A")
-C = string.count("C")
-G = string.count("G")
-T = string.count("T")
-total = A+C+G+T
-print("A -",A,"\nC -",C,"\nG -",G,"\nT -",T,"\nTotal -",total)
