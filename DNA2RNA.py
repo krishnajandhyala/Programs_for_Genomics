b28f8fbd0ec8c1dee57bca8d531e0f45d8c803d7
Jandhyala Sai Krishna <119666181+krishnajandhyala@users.noreply.github.com>
2023-02-24 16:19:13 +0530

    Add files via upload

---
+++ DNA2RNA.py
@@ -0,0 +1,18 @@
+with open("test.fasta", "r") as f:
+    sequences = {}
+    current_sequence = ""
+    for line in f:
+        if line.startswith(">"):
+            if current_sequence:
+                sequences[header] = current_sequence
+            header = line.strip()[1:]
+            current_sequence = ""
+        else:
+            current_sequence += line.strip()
+    sequences[header] = current_sequence
+with open("output.fasta", "w") as f:
+    for header, sequence in sequences.items():
+        rna_sequence = sequence.replace("T", "U")
+        f.write(">" + header + "\n")
+        for i in range(0, len(rna_sequence), 100):
+            f.write(rna_sequence[i:i+100] + "\n")
