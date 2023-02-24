cc23a81bb8fa73ce0dd1ee12d95d24d130cb033d
Jandhyala Sai Krishna <119666181+krishnajandhyala@users.noreply.github.com>
2023-02-24 17:08:30 +0530

    Update reverse_complement.py

--- reverse_complement.py
+++ reverse_complement.py
@@ -1,22 +1,12 @@
-with open(input("Enter fasta file path \t:\t"), "r") as f:
-    sequences = {}
-    current_sequence = ""
+#Usage: python3 reverse_complement.py > output_RC.fasta
+
+with open(input("Enter multifasta file path\t:\t")) as f:
     for line in f:
         if line.startswith(">"):
-            if current_sequence:
-                sequences[header] = current_sequence
-            header = line.strip()[1:]
-            current_sequence = ""
+            print(line.strip())
         else:
-            current_sequence += line.strip()
-    sequences[header] = current_sequence
-with open("output.fasta", "w") as f:
-    for header, sequence in sequences.items():
-        comp1 = sequence.replace("A","I").replace("G","J")
-        comp2 = comp1.replace("C","G").replace("T","A")
-        comp3 = comp2.replace("I","T").replace("J","C")
-        reversecomplement = comp3[::-1]
-        f.write(">" + header + "\n")
-        for i in range(0, len(comp3), 100):
-            f.write(comp3[i:i+100] + "\n")
-
+            comp1 = line.replace("A","I").replace("G","J")
+            comp2 = comp1.replace("C","G").replace("T","A")
+            comp3 = comp2.replace("I","T").replace("J","C")
+            reversecomplement = comp3[::-1]
+            print(reversecomplement)
