78d8472a7e0f03dbf8fa1b0238c40d0bab3f3b3b
Jandhyala Sai Krishna <119666181+krishnajandhyala@users.noreply.github.com>
2023-02-24 17:16:06 +0530

    Update nuc_counter.py

--- nuc_counter.py
+++ nuc_counter.py
@@ -1,27 +1,15 @@
-#file = input("Enter your file path\t:\t")
-#string = open(file).readlines()
-
-#A = string.count("A")
-#C = string.count("C")
-#G = string.count("G")
-#T = string.count("T")
-#total = A+C+G+T
-#print("A -",A,"\nC -",C,"\nG -",G,"\nT -",T,"\nTotal -",total)
-
-
-import re
-
+##Usage: python3 nuc_counter.py > output_num.txt
 A=T=G=C=N=0
 
-with open(input("Give your file name/path \t:\t")) as fh:
-	#header = re.findall(">", fh)
-	for line in fh:
-		A += line.count("A")
-		T += line.count("T")
-		G += line.count("G")
-		C += line.count("C")
-		N += line.count("N")
-	total = A+T+G+C+N
-
+with open(input("Enter fasta file path \t:\t"), "r") as f:
+        for line in f:
+                if line.startswith(">"):
+                    print
+                else:
+                    A += line.count("A")
+                    T += line.count("T")
+                    G += line.count("G")
+                    C += line.count("C")
+                    N += line.count("N")
+                    total = A+T+G+C+N
 print("A -",A,"\nC -",C,"\nG -",G,"\nT -",T,"\nN -",N,"\nTotal -",total)
-
