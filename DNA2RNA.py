dc33278d41b208220883c7a4ed69f47716c80807
Jandhyala Sai Krishna <119666181+krishnajandhyala@users.noreply.github.com>
2023-02-24 16:59:21 +0530

    Update DNA2RNA.py

--- DNA2RNA.py
+++ DNA2RNA.py
@@ -1,18 +1,9 @@
-with open(input("Enter the path of the multifasta file \t:\t"), "r") as f:
-    sequences = {}
-    current_sequence = ""
+#Usage python3 DNA2RNA.py > output_RNA.fasta
+
+with open("test.fasta", "r") as f:
     for line in f:
         if line.startswith(">"):
-            if current_sequence:
-                sequences[header] = current_sequence
-            header = line.strip()[1:]
-            current_sequence = ""
+            print(line.strip())
         else:
-            current_sequence += line.strip()
-    sequences[header] = current_sequence
-with open("output_DNA2RNA.fasta", "w") as f:
-    for header, sequence in sequences.items():
-        rna_sequence = sequence.replace("T", "U")
-        f.write(">" + header + "\n")
-        for i in range(0, len(rna_sequence), 100):
-            f.write(rna_sequence[i:i+100] + "\n")
+            rna_sequence = line.strip().replace("T", "U")
+            print(rna_sequence)
