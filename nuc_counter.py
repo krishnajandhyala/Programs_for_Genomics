00f356a42152bdc35083eb0731bac1b930ac4cd0
Jandhyala Sai Krishna <119666181+krishnajandhyala@users.noreply.github.com>
2023-02-24 17:17:02 +0530

    Update nuc_counter.py

--- nuc_counter.py
+++ nuc_counter.py
@@ -4,7 +4,7 @@ A=T=G=C=N=0
 with open(input("Enter fasta file path \t:\t"), "r") as f:
         for line in f:
                 if line.startswith(">"):
-                    print()
+                    pass
                 else:
                     A += line.count("A")
                     T += line.count("T")
